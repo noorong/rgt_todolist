90921e3e2802e802e2d9b8c483f9b325ffceee17
noorong <100012522+noorong@users.noreply.github.com>
2023-04-01 20:37:20 +0900

    feat : 입력칸 생성 , 서버에서 받아왔다고 가정한 데이터를 렌더링

--- ToDoList.js
+++ ToDoList.js
@@ -1,12 +1,21 @@
-const inputList = document.querySelector(".inputList")
+const inputBar = document.querySelector(".inputBar")
+const userList = [0,1,2,3,4]
+const todoList = document.querySelector(".todoList");
 
-
-inputList.addEventListener("keyup", function (e) {
-    if (e.keyCode === 13) {
-        asda(e)
-    }
-  });
-
-function asda(e){ 
-    console.log(e.target.value)
+for (let i = 0; i < userList.length; i++) {
+    let child = document.createElement("input")
+    child.value = userList[i]
+    let childLi = document.createElement("li")
+    childLi.appendChild(child)
+    todoList.appendChild(childLi)
 }
+
+// inputBar.addEventListener("keyup", function (e) {
+//     if (e.keyCode === 13) {
+//         insertList(e)
+//     }
+//   });
+
+// function insertList(e) { 
+//     console.log(e.target.value)
+// }
