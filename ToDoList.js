03dfe0550a67bc281e99715763f8b50a25f2eee3
noorong <100012522+noorong@users.noreply.github.com>
2023-04-02 16:25:24 +0900

    feat : 엔터시 등록하기, 5개까지만 등록가능, 중복되는 코드 모듈화필요

--- ToDoList.js
+++ ToDoList.js
@@ -1,24 +1,36 @@
-const inputBar = document.querySelector(".inputBar")
-let userList = [0,1,2,3,4]
+const inputBar = document.querySelector(".inputBar");
+let userList = ["서류 전달하기"];
 const todoList = document.querySelector(".todoList");
 const clrBtn = document.querySelector(".clrBtn");
 
-clrBtn.addEventListener('click', () => todoList.innerHTML= "")
+clrBtn.addEventListener("click", () => (todoList.innerHTML = ""));
 
 for (let i = 0; i < userList.length; i++) {
-    let child = document.createElement("input")
-    child.value = userList[i]
-    let childLi = document.createElement("li")
-    childLi.appendChild(child)
-    todoList.appendChild(childLi)
+  let child = document.createElement("input");
+  child.value = i + 1 + "." + userList[i];
+  let childLi = document.createElement("li");
+  childLi.appendChild(child);
+  todoList.appendChild(childLi);
 }
 
-// inputBar.addEventListener("keyup", function (e) {
-//     if (e.keyCode === 13) {
-//         insertList(e)
-//     }
-//   });
+inputBar.addEventListener("keyup", function (e) {
+  if (userList.length < 5) {
+    if (e.keyCode === 13) {
+      insertList(e);
+      inputBar.value = "";
+    }
+  } else {
+    alert("5개 이상 등록할 수 없습니다.");
+    inputBar.value = "";
+  }
+});
 
-// function insertList(e) { 
-//     console.log(e.target.value)
-// }
+function insertList(e) {
+  userList.push(e.target.value);
+
+  let child = document.createElement("input");
+  child.value = userList[userList.length - 1];
+  let childLi = document.createElement("li");
+  childLi.appendChild(child);
+  todoList.appendChild(childLi);
+}
