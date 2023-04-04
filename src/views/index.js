f5572b26e16a1f4946ae8ff6b6b00a8656d85919
noorong <100012522+noorong@users.noreply.github.com>
2023-04-04 21:58:12 +0900

    debug: 하나의 ol리스트에선 정상동작

--- src/views/index.js
+++ src/views/index.js
@@ -1,4 +1,4 @@
-let todoList =  ["서류 전달하기","서류 전달하기","서류 전달하기"];
+let todoList = ["서류 1", "서류 2", "서류 3"];
 const inputBar = document.querySelector(".inputBar");
 const olList = document.querySelector(".olList");
 const clrBtn = document.querySelector(".clrBtn");
@@ -7,24 +7,20 @@ clrBtn.addEventListener("click", () => {
   olList.innerHTML = "";
   todoList = [];
 });
+
 renderList();
+changeColorForExistingLists();
 
 //list 등록
 inputBar.addEventListener("keyup", function (e) {
   if (todoList.length < 5) {
     if (e.keyCode === 13) {
       if (e.target.value !== "") {
-        olList.innerHTML = ""
-        todoList.unshift(e.target.value);
-        renderList();
-        const selectElements = document.querySelectorAll('select.progress');
-        selectElements.forEach(selectElement => {
-          selectElement.addEventListener('change', (event) => {
-            const selectedValue = event.target.value;
-            const divElement = event.target.parentNode;
-            changeColor(selectedValue, divElement);
-          });
-        });
+        todoList.push(e.target.value);
+        renderNewList();
+        const selectElement = document.querySelector("select.progress");
+        const divElement = selectElement.parentNode;
+        changeColorForNewList(divElement);
       }
       inputBar.value = "";
     }
@@ -34,17 +30,37 @@ inputBar.addEventListener("keyup", function (e) {
   }
 });
 
+//새로운 리스트 추가
+function renderNewList() {
+  if (todoList[todoList.length -1] !== "") {
+    const divElement = document.createElement("div");
+    divElement.innerHTML = `
+      <select class="progress">
+        <option value="">진행상황</option>
+        <option value="Done">Done</option>
+        <option value="Ongoing">Ongoing</option>
+        <option value="Delay">Delay</option>
+      </select>
+      <li>${todoList[todoList.length -1]}</li>  
+    `;
+    olList.insertBefore(divElement, olList.lastChild);
+    changeColorForNewList(divElement); 
+  }
+}
+
+
 //받아온 데이터 li에 정보 삽입
-function renderList(){
+function renderList() {
+  olList.innerHTML = "";
   for (let i = 0; i < todoList.length; i++) {
-    if(todoList[i] !== ""){
-    olList.insertAdjacentHTML(
-      "afterbegin",
-      `
+    if (todoList[i] !== "") {
+      olList.insertAdjacentHTML(
+        "beforeend",
+        `
       <div>
         <select class="progress">
           <option value="">진행상황</option>
-          <option value="Done" >Done</option>
+          <option value="Done">Done</option>
           <option value="Ongoing">Ongoing</option>
           <option value="Delay">Delay</option>
         </select>
@@ -52,8 +68,32 @@ function renderList(){
       </div>       
       `
       );
+    }
   }
-  }
+}
+
+// 기존의 리스트의 배경색을 유지
+function changeColorForExistingLists() {
+  const selectElements = document.querySelectorAll("select.progress");
+  selectElements.forEach((selectElement) => {
+    selectElement.addEventListener("change", (event) => {
+      const selectedValue = event.target.value;
+      const divElement = event.target.closest("div");
+      changeColor(selectedValue, divElement);
+    });
+    const selectedValue = selectElement.value;
+    const divElement = selectElement.closest("div");
+    changeColor(selectedValue, divElement);
+  });
+}
+
+// 새로운 리스트의 배경색을 변경
+function changeColorForNewList(divElement) {
+  const selectElement = divElement.querySelector("select.progress");
+  selectElement.addEventListener("change", (event) => {
+    const selectedValue = event.target.value;
+    changeColor(selectedValue, divElement);
+  });
 }
 
 //사용자 이름 등록
@@ -65,25 +105,26 @@ userName.addEventListener("keyup", function (e) {
     userName.setAttribute("disabled", "true");
     userColumn.children[1].removeAttribute("disabled");
     userColumn.children[1].focus();
-  }  
+  }
 });
 
-//진행상황 체크,
-const selectElements = document.querySelectorAll('select.progress');
+// 진행상황 체크
+const selectElements = document.querySelectorAll("select.progress");
 selectElements.forEach(selectElement => {
-  selectElement.addEventListener('change', (event) => {
-  const selectedValue = event.target.value;
-  const divElement = event.target.parentNode;
-  changeColor(selectedValue,divElement);
+  selectElement.addEventListener("change", (event) => {
+    const selectedValue = event.target.value;
+    const divElement = event.target.parentNode;
+    changeColor(selectedValue, divElement);
   });
 });
 
-function changeColor(selectedValue,divElement) {
+// changeColor 함수
+function changeColor(selectedValue, divElement) {
   switch (selectedValue) {
-    case 'Done':
-      divElement.style.backgroundColor = 'greenyellow';
+    case "Done":
+      divElement.style.backgroundColor = "greenyellow";
       break;
-    case 'Ongoing':
+    case "Ongoing":
       divElement.style.backgroundColor = 'orange';
       break;
     case 'Delay':
